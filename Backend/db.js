469a6c395020d7d56c4cf26bc597819781119957
shreejadhav007 <104477778+shreejadhav007@users.noreply.github.com>
2024-01-29 13:35:41 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb://localhost:27017/TODO?directConnection=true";
-// const uri = "mongodb+srv://trendhub:admin@todo.bzgw1rz.mongodb.net/"
+// const uri = "mongodb://localhost:27017/TODO?directConnection=true";
+ const uri = "mongodb+srv://trendhub:admin@todo.bzgw1rz.mongodb.net/"
 async function main(){
     await mongoose.connect(uri);
     console.log("connected successfully")
